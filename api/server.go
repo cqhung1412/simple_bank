4b94bf807b04b540f46ef415af665081a62f47bf
hungcq3 <hungcq3@vng.com.vn>
2022-09-16 11:53:21 +0700

    add login function

--- api/server.go
+++ api/server.go
@@ -1,26 +1,27 @@
 package api
 
 import (
+	"fmt"
+
 	db "github.com/cqhung1412/simple_bank/db/sqlc"
+	"github.com/cqhung1412/simple_bank/token"
+	"github.com/cqhung1412/simple_bank/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
+func (server *Server) setupRouter() {
 	router := gin.Default()
-
-	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		v.RegisterValidation("currency", validateCurrency)
-	}
-
 	router.POST("/user", server.createUser)
+	router.POST("/users/login", server.loginUser)
 
 	router.POST("/account", server.createAccount)
 	router.GET("/accounts", server.listAccounts)
@@ -30,7 +31,26 @@ func NewServer(store db.Store) *Server {
 	router.POST("/transfer", server.createTransfer)
 
 	server.router = router
-	return server
+}
+
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	// tokenMaker, err := token.NewJWTMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validateCurrency)
+	}
+
+	server.setupRouter()
+	return server, nil
 }
 
 func errorResponse(err error) gin.H {
